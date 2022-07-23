6bdf101f2dfe1dde970ace0143e2e6e527aff85d
Joseph Finlayson <joseph.finlayson@gmail.com>
2022-07-23 17:13:46 +0200

    refactor

--- examples/phat/weather-phat.py
+++ examples/phat/weather-phat.py
@@ -15,7 +15,7 @@ from font_fredoka_one import FredokaOne
 from font_source_sans_pro import SourceSansProSemibold
 from inky.auto import auto
 from PIL import Image, ImageDraw, ImageFont
-
+from grid import Box
 # Get the current path
 PATH = os.path.dirname(__file__)
 
@@ -172,41 +172,6 @@ i = 0
 grids = []
 
 # Box stores the four coordinates of the box, top left, top right, bottom left and bottom right
-
-
-class Box:
-    def __init__(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
-
-    def __str__(self):
-        return "({}, {}, {}, {})".format(self.x1, self.y1, self.x2, self.y2)
-
-    def __repr__(self):
-        return self.__str__()
-
-    def top_left(self):
-        return self.x1, self.y1
-
-    def top_right(self):
-        return self.x2, self.y1
-
-    def bottom_left(self):
-        return self.x1, self.y2
-
-    def bottom_right(self):
-        return self.x2, self.y2
-
-    def center(self):
-        return int((self.x1 + self.x2) / 2), int((self.y1 + self.y2) / 2)
-
-    def width(self):
-        return self.x2 - self.x1
-
-    def height(self):
-        return self.y2 - self.y1
 
 
 # iterate over width divided by 3
